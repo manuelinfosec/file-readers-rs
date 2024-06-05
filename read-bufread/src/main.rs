01fb326f3a5bd609cb81e5d92db4cc4c6828ad0b
Manuel <manuelinfosec@gmail.com>
2024-06-05 20:16:26 +0100

    bufread: Reading file and metadata

--- read-bufread/src/main.rs
+++ read-bufread/src/main.rs
@@ -1,3 +1,15 @@
+use std::env::args;
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = args().collect();
+    let path: &String = &args[1];
+
+    let file = File::open(path).expect("Unable to open file.");
+    let file_length: usize = file
+        .metadata()
+        .expect("Unable to query file details")
+        .len()
+        .try_into()
+        .expect("Cannot convert file length");
 }
